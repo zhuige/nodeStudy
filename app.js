4af32518e07739234c390d2385266e677f4ce6a5
liangjunjie-home <792972988@qq.com>
2019-11-11 23:29:58 +0800

    '全局报错信息'

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const Koa = require("koa");
-const InitManager = require("./core/init");
-const parser = require("koa-bodyparser");
+const Koa = require('koa');
+const InitManager = require('./core/init');
+const parser = require('koa-bodyparser');
+const catchError = require('./middlewares/exception');
 
 const app = new Koa();
 app.use(parser());
 InitManager.initCore(app);
+app.use(catchError);
 
 app.listen(3000);
